92d2441a58f3643d234348efe47846481135eb50
robinbenitezmora <robinbenitez1990@gmail.com>
2023-07-14 22:37:47 -0500

    Update the code

--- CurrencyConverter.java
+++ CurrencyConverter.java
@@ -16,67 +16,128 @@ public class CurrencyConverter {
         "Exit"
     };
 
-    String input = (String) JOptionPane.showInputDialog(null, "Choose now...", "Menu",
-        JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]); // Initial choice
+    String input = (String) JOptionPane.showInputDialog(
+        null,
+        "Choose now...",
+        "Menu",
+        JOptionPane.QUESTION_MESSAGE,
+        null,
+        choices,
+        choices[0]);
 
-    while (input != "Exit") {
-      if (input == "Dollars to Pesos") {
+    while (!input.equals("Exit")) {
+      if (input.equals("Dollars to Pesos")) {
         String dollars = JOptionPane.showInputDialog("Enter the amount of dollars");
         double dollars2 = Double.parseDouble(dollars);
         double pesos = dollars2 * 4500;
-        JOptionPane.showMessageDialog(null, "The amount of pesos is " + pesos);
-      } else if (input == "Pesos to Dollars") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of pesos is " + pesos,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Pesos to Dollars")) {
         String pesos = JOptionPane.showInputDialog("Enter the amount of pesos");
         double pesos2 = Double.parseDouble(pesos);
         double dollars = pesos2 / 4500;
-        JOptionPane.showMessageDialog(null, "The amount of dollars is " + dollars);
-      } else if (input == "Euros to Pesos") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of dollars is " + dollars,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Euros to Pesos")) {
         String euros = JOptionPane.showInputDialog("Enter the amount of euros");
         double euros2 = Double.parseDouble(euros);
         double pesos = euros2 * 5000;
-        JOptionPane.showMessageDialog(null, "The amount of pesos is " + pesos);
-      } else if (input == "Pesos to Euros") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of pesos is " + pesos,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Pesos to Euros")) {
         String pesos = JOptionPane.showInputDialog("Enter the amount of pesos");
         double pesos2 = Double.parseDouble(pesos);
         double euros = pesos2 / 5000;
-        JOptionPane.showMessageDialog(null, "The amount of euros is " + euros);
-      } else if (input == "Esterlines to Pesos") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of euros is " + euros,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Esterlines to Pesos")) {
         String esterlines = JOptionPane.showInputDialog("Enter the amount of esterlines");
         double esterlines2 = Double.parseDouble(esterlines);
         double pesos = esterlines2 * 5400;
-        JOptionPane.showMessageDialog(null, "The amount of pesos is " + pesos);
-      } else if (input == "Pesos to Esterlines") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of pesos is " + pesos,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Pesos to Esterlines")) {
         String pesos = JOptionPane.showInputDialog("Enter the amount of pesos");
         double pesos2 = Double.parseDouble(pesos);
         double esterlines = pesos2 / 5400;
-        JOptionPane.showMessageDialog(null, "The amount of esterlines is " + esterlines);
-      } else if (input == "Yens to Pesos") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of esterlines is " + esterlines,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Yens to Pesos")) {
         String yens = JOptionPane.showInputDialog("Enter the amount of yens");
         double yens2 = Double.parseDouble(yens);
         double pesos = yens2 * 28.9;
-        JOptionPane.showMessageDialog(null, "The amount of pesos is " + pesos);
-      } else if (input == "Pesos to Yens") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of pesos is " + pesos,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Pesos to Yens")) {
         String pesos = JOptionPane.showInputDialog("Enter the amount of pesos");
         double pesos2 = Double.parseDouble(pesos);
         double yens = pesos2 / 28.9;
-        JOptionPane.showMessageDialog(null, "The amount of yens is " + yens);
-      } else if (input == "Won to Pesos") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of yens is " + yens,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Won to Pesos")) {
         String won = JOptionPane.showInputDialog("Enter the amount of won");
         double won2 = Double.parseDouble(won);
         double pesos = won2 * 0.31;
-        JOptionPane.showMessageDialog(null, "The amount of pesos is " + pesos);
-      } else if (input == "Pesos to Won") {
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of pesos is " + pesos,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
+      } else if (input.equals("Pesos to Won")) {
         String pesos = JOptionPane.showInputDialog("Enter the amount of pesos");
         double pesos2 = Double.parseDouble(pesos);
         double won = pesos2 / 0.31;
-        JOptionPane.showMessageDialog(null, "The amount of won is " + won);
+        JOptionPane.showMessageDialog(
+            null,
+            "The amount of won is " + won,
+            "Conversion Result",
+            JOptionPane.INFORMATION_MESSAGE);
       } else {
-        JOptionPane.showMessageDialog(null, "Invalid option");
+        JOptionPane.showMessageDialog(
+            null,
+            "Invalid option",
+            "Error",
+            JOptionPane.ERROR_MESSAGE);
       }
-      input = (String) JOptionPane.showInputDialog(null, "Choose now...", "The Choice of a Lifetime",
-          JOptionPane.QUESTION_MESSAGE, null, choices, choices[0]);
+
+      input = (String) JOptionPane.showInputDialog(
+          null,
+          "Choose now...",
+          "The Choice of a Lifetime",
+          JOptionPane.QUESTION_MESSAGE,
+          null,
+          choices,
+          choices[0]);
     }
 
-    JOptionPane.showMessageDialog(null, "Thanks for using the application");
+    JOptionPane.showMessageDialog(
+        null,
+        "Thanks for using the application",
+        "Goodbye",
+        JOptionPane.INFORMATION_MESSAGE);
   }
 }
